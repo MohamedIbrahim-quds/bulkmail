6affe7c050c0e7907f6d0b5a610237290ade8c5f
Mohamed Ibrahim <84565637+MohamedIbrahim-quds@users.noreply.github.com>
2021-08-09 06:59:36 +0530

    Add files via upload

--- JavaSendMail.java
+++ JavaSendMail.java
@@ -13,45 +13,50 @@ import javax.mail.internet.MimeMessage;
 public class JavaSendMail {
 	public static void sendmail(String recepient) {
 		System.out.println("preparing to send email...");
-		
-		Properties properties =new Properties();
+
+		Properties properties = new Properties();
 		properties.put("mail.smtp.auth", "true");
 		properties.put("mail.smtp.starttls.enable", "true");
 		properties.put("mail.smtp.host", "smtp.gmail.com");
 		properties.put("mail.smtp.port", "587");
-		
-		String username="mnmirockzz267@gmail.com";
-		String password="saramowafica";
-		
-		Session session= Session.getInstance(properties, new Authenticator() {
+
+		String username = "mnmirockzz267@gmail.com";
+		String password = "saramowafica";
+
+		Session session = Session.getInstance(properties, new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				// TODO Auto-generated method stub
-				return new PasswordAuthentication(username,password);
+				return new PasswordAuthentication(username, password);
 			}
 		});
-		
-		Message message=prepareMessage(session,username,recepient);
+
+		Message message = prepareMessage(session, username, recepient);
+
 		try {
 			Transport.send(message);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		System.out.println("email sent succesfully to "+recepient);
+		System.out.println("email sent succesfully to " + recepient);
 	}
-	private static Message prepareMessage(Session session, String username, String recepient) {
-		Message message= new MimeMessage(session);
+
+	public static Message prepareMessage(Session session, String username, String recepient) {
+
+		String[] splitted = recepient.split("@");
+		String name = splitted[0];
+
+		Message message = new MimeMessage(session);
 		try {
 			message.setFrom(new InternetAddress(username));
 			message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
 			message.setSubject("[....testing mail....]");
-			message.setText(("hi,\n [......mail index....]"));
+			message.setText("Dear " + name + ","
+							+ "\nWelcome to my Github Account-  https://github.com/MohamedIbrahim-quds");
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
 		return message;
 	}
-
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,74 @@
 package bulkmail;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.xml.crypto.Data;
+
 
 public class Main {
 	public static void main(String[] args) {
-		JavaSendMail.sendmail("mnmirockzz267@gmail.com");
+		JavaSendMail mailsender = new JavaSendMail();
+		Set<String> emails = get();
+		for(String string : emails) {
+	mailsender.sendmail(string);
+		//Iterator<String> iter= Data.iterator();		
+//	while (iter.hasNext()) {
+	//		String toEmail = iter.next();
+		//	mailSender.startProcess(toEmail);
+		}
 	}
+		
+		public static  Set<String>  get(){
+			Set<String> emails=new HashSet<>();
+			emails.add("mnmirockzz267@gmail.com");
+			emails.add("aakreety07@hotmail.com");
+			emails.add("amyslax@hotmail.com");
+			emails.add("aniliuk@hotmail.com");
+			emails.add("anisha_shrestha2@hotmail.com");
+			emails.add("asajeshani@hotmail.co.uk");
+			emails.add("aslam1qqqq@gmail.com");			
+			emails.add("barunshrestha@hotmail.com");
+			emails.add("beyounce_ranzita@hotmail.com");
+			emails.add("bhimrajg@hotmail.com");
+			emails.add("bijay_kc@hotmail.com");
+			
+			emails.add("bivek_shrestha@hotmail.com");
+			emails.add("bloody_tears@live.com");
+			emails.add("borderline_bipolar@btopenworld.com");
+			emails.add("boyz_eitan@hotmail.com");
+			emails.add("coolpurna@hotmail.com");
+			emails.add("danger_don45@hotmail.com");
+			emails.add("dcblmc_065823@hotmail.com");
+			emails.add("debbierana@hotmail.com");
+			emails.add("devasainju@msn.com");
+			emails.add("devil_tanisha@hotmail.com");
+			
+			emails.add("hell_pursuit@hotmail.com");
+			emails.add("hill_tiger2007@hotmail.com");
+			emails.add("jadewyles@hotmail.co.uk");
+			emails.add("jawad_drmc@yahoo.com");
+			emails.add("jericho_kritisha_peterson@hotmail.com");
+			emails.add("jhonradhae@hotmail.com");
+			emails.add("k_pant@hotmail.com");
+			emails.add("kanekanef_eminem@hotmail.com");
+			emails.add("kc.ka@hotmail.com");
+			emails.add("kg_luckycharm@hotmail.com");
+		
+	
 
+
+	
+
+	
+
+
+			return emails;
 }
+}
+
+		
+	
+
+
